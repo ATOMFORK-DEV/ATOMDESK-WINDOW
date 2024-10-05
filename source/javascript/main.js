631e04ca3d115fd7876e01c65e62e1a9c7d2a244
ᗩᴛᴏᴍғᴏʀᴋ ᗪᴇᴠᴇʟᴏᴘᴍᴇɴᴛ ™ <ATOMFORK.DEV@GMAIL.COM>
2024-10-05 07:10:42 -0600

    DEV COMMIT
    BUILD THE TITLEBAR

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,32 +1,58 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
 const path = require('node:path')
 
+let win;
+
 function createWindow () {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
     },
-    
     icon: path.join(__dirname, '..', 'assets', 'favicon', 'favicon.ico')
   })
 
   win.loadFile(path.join(__dirname, '..', 'index.html'))
+
+  win.on('maximize', () => {
+    win.webContents.send('window-maximized');
+  });
+
+  win.on('unmaximize', () => {
+    win.webContents.send('window-unmaximized');
+  });
 }
 
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+ipcMain.on('minimize-window', () => {
+  win.minimize();
+});
+
+ipcMain.on('maximize-window', () => {
+  win.maximize();
+});
+
+ipcMain.on('unmaximize-window', () => {
+  win.unmaximize();
+});
+
+ipcMain.on('close-window', () => {
+  win.close();
+});
